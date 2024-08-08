799add451c25eef5d5ea338a6a3de86d67dab6ad
SwanFlutter <swan.dev1993@gmail.com>
2024-08-07 23:24:31 -0700

    u

--- lib/flutter_saver.dart
+++ lib/flutter_saver.dart
@@ -88,7 +88,7 @@ class FlutterSaver {
       throw Exception('Storage permission not granted');
     }
     String filePath = '';
-    String localFileName = "iamage_$randomFileName";
+    String localFileName = "iamage_$randomFileName($lengthFileName)";
 
     String finalFilename = fileName ?? localFileName;
 
