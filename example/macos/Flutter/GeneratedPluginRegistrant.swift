e7370ed0acc02494c34942aa0e57e80e7572efa0
Romavic dos Anjos <romavicdosanjoskc@gmail.com>
2024-09-17 05:05:58 +0100

    add project example

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import external_path_ios_mac
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  ExternalPathIosMacPlugin.register(with: registry.registrar(forPlugin: "ExternalPathIosMacPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
